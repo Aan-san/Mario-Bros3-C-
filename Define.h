6b882869e48f0a01b46d79e3a33e02560c397d19
17520221 <17520221@gm.uit.edu.vn>
2021-07-22 22:52:50 +0700

    update fireball skill

---
+++ Define.h
@@ -0,0 +1,13 @@
+#pragma once
+//status board
+#define HUD_HEIGHT 32
+
+//windown
+#define WINDOW_CLASS_NAME L"SampleWindow"
+#define MAIN_WINDOW_TITLE L"Mario bros 3 "
+
+#define BACKGROUND_COLOR D3DCOLOR_XRGB(0, 0, 0)
+#define SCREEN_WIDTH 272
+#define SCREEN_HEIGHT 256
+
+#define MAX_FRAME_RATE 144
--- main.cpp
+++ main.cpp
@@ -26,14 +26,7 @@
 
 #include "PlayScence.h"
 
-#define WINDOW_CLASS_NAME L"SampleWindow"
-#define MAIN_WINDOW_TITLE L"SAMPLE 05 - SCENCE MANAGER"
 
-#define BACKGROUND_COLOR D3DCOLOR_XRGB(0, 153, 255)
-#define SCREEN_WIDTH 320
-#define SCREEN_HEIGHT 240
-
-#define MAX_FRAME_RATE 120
 
 CGame *game;
 
