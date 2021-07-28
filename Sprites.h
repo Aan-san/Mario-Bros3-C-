c52c0a52094abe21c85dc637846f90e1d9c5d8a9
17520221 <17520221@gm.uit.edu.vn>
2021-07-28 09:53:05 +0700

    hud nad font

--- Sprites.h
+++ Sprites.h
@@ -9,35 +9,34 @@ class CSprite
 {
 	int id;				// Sprite ID in the sprite database
 
-	int left; 
+	int left;
 	int top;
 	int right;
 	int bottom;
 
 	LPDIRECT3DTEXTURE9 texture;
-public: 
+public:
 	CSprite(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex);
 
 	void Draw(float x, float y, int alpha = 255);
 };
 
-typedef CSprite * LPSPRITE;
+typedef CSprite* LPSPRITE;
 
 /*
 	Manage sprite database
 */
 class CSprites
 {
-	static CSprites * __instance;
-
-	unordered_map<int, LPSPRITE> sprites;
+	static CSprites* __instance;
 
 public:
+	unordered_map<int, LPSPRITE> sprites;
 	void Add(int id, int left, int top, int right, int bottom, LPDIRECT3DTEXTURE9 tex);
 	LPSPRITE Get(int id);
 	void CSprites::Clear();
 
-	static CSprites * GetInstance();
+	static CSprites* GetInstance();
 };
 
 
