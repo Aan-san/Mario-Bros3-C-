f2183eda3b6a9cbdb88f5a773244daee994e7340
17520221 <17520221@gm.uit.edu.vn>
2021-07-28 13:12:41 +0700

    update score class

--- Portal.cpp
+++ Portal.cpp
@@ -1,24 +1,44 @@
 #include "Portal.h"
+#include "PlayScence.h"
 
-
-CPortal::CPortal(float l, float t, float r, float b, int scene_id )
+CPortal::CPortal(int scene_id, float start_x, float start_y)
 {
 	this->scene_id = scene_id;
-	x = l; 
-	y = t;
-	width = r - l + 1;
-	height = b - t + 1;
+	this->start_x = start_x;
+	this->start_y = start_y;
+	type = IGNORE;
 }
 
 void CPortal::Render()
 {
 	RenderBoundingBox();
 }
+void CPortal::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	CMario* mario = ((CPlayScene*)CGame::GetInstance()->GetCurrentScene())->GetPlayer();
+	float mLeft, mTop, mRight, mBottom;
+	float oLeft, oTop, oRight, oBottom;
+	if (mario != NULL)
+	{
+		mario->GetBoundingBox(mLeft, mTop, mRight, mBottom);
+		GetBoundingBox(oLeft, oTop, oRight, oBottom);
+		if (isColliding(floor(mLeft), mTop, ceil(mRight), mBottom) && mario->isWannaDown
+			&& mLeft >= oLeft && mRight <= oRight)
+		{
+			mario->portal = this;
+			if (tag == BACKTOPLAYSCENE)
+				mario->StartPipeUp();
+			if (tag == TOEXTRASCENE)
+				mario->StartPipeDown();
+			return;
+		}
 
-void CPortal::GetBoundingBox(float &l, float &t, float &r, float &b)
+	}
+}
+void CPortal::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
 	l = x;
 	t = y;
-	r = x + width;
-	b = y + height;
+	r = x + PORTAL_BBOX_WIDTH;
+	b = y + PORTAL_BBOX_HEIGHT;
 }
